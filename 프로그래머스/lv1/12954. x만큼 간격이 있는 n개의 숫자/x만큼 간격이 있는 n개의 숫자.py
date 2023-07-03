96d4c1adb6b574307e365ac85a17d451bba2fc87
Minhee <49539711+minkim7704@users.noreply.github.com>
2023-07-03 19:16:18 +0900

    Update x만큼 간격이 있는 n개의 숫자.py

--- 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
+++ 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
@@ -3,3 +3,8 @@ def solution(x, n):
     for i in range(1, n + 1):
         answer.append(x*i)
     return answer
+
+
+def solution(x, n):
+    answer = [i * x for i in range(1, n + 1)]
+    return answer
