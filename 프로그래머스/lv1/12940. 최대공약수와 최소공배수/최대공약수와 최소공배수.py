1a06d60cbcdc2a69a5f6fdc1114fb3a4c00dd51c
Minhee <49539711+minkim7704@users.noreply.github.com>
2023-07-04 19:28:02 +0900

    Update 최대공약수와 최소공배수.py

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,7 +1,7 @@
-
-# 두 수의 최대 공약수 구하기
+# 두 수의 최대 공약수, 최소 공배수 구하기
 def solution(n, m):
     answer = []
+    # 최대 공약수 구하기
     for i in range(min(n, m), 0, -1):
     # range(min(n, m), 0, -1) - 두 수 중 더 작은 숫자부터 0까지, -1만큼 내려가면서 for-loop을 돌리겠다.
         if n % i == 0 and m % i == 0:
@@ -11,6 +11,8 @@ def solution(n, m):
             # 공약수를 하나 찾고 나면 for-loop을 빠져나온다.
             # 왜냐하면 최대 공약수를 찾으려고 일부러 주어진 수부터 1까지 돌아가는 for-loop을 돌린거니까
             # 제일 먼저 나온 첫 숫자가 두 수의 최대 공약수이다.
+    
+    # 최소 공배수 구하기
     for i in range(max(n, m), (n * m) + 1):
     # n과 m 중 더 큰 숫자부터 시작해서 n * m + 1한 숫자까지 for-loop을 돌린다.
     # 최대 공약수에서는 큰 수를 찾는 것이라 큰 수부터 1까지 내려왔지만
@@ -25,8 +27,22 @@ def solution(n, m):
             # answer.append(i) - 각각의 for-loop에서 원하는 최대 공약수와 최소 공배수를
             # 차례대로 넣은 answer list 반환!
 
+    
 # 최대 공약수, 최소 공배수를 찾는 엄청 편리한 방법을 찾았당
 # 최소 공배수는 호출이 안되지만 그래도 메모메모
+    
 # import math
 # print(math.gcd(3, 12))
 # print(math.lcm(3, 12))
+
+
+
+# 이건 그냥 한번 더 리뷰
+# 정수 n의 약수 구하기
+def solution(n):
+    answer = []
+    for i in range(1, n + 1):
+        if n % i == 0:          # n을 i로 나눠서 나머지가 0이면 i는 n의 약수다.
+            answer.append(i)
+    return answer
+print(solution(12))
