9574d2e40bf5acebcd124eb0bb514d4dbac25e14
Minhee <49539711+minkim7704@users.noreply.github.com>
2023-07-06 18:56:53 +0900

    Update 하샤드 수.py

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
@@ -12,3 +12,18 @@ def solution(x):            # x의 각 자릿수 합을 구한다.
         return True         
     else:
         return False
+
+
+
+
+# 다른 방법
+def Harshad(x):
+    return x%sum(int(n) for n in str(x)) == 0
+                 # 1. 정수의 각 자릿수 합을 구한다 = str(x)로 for-loop 돌려서 나온 n
+             # 2. 그 n을 int로 변환한 합으로
+        # 3. x를 나누어주었을때
+                                      # 4. 나머지가 0이면
+    # 5. 리턴~ (맞으면 True 틀리면 False)
+    
+
+# ㅎㅎ 우왕
