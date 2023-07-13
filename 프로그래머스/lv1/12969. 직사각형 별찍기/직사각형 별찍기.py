c918ecda6ad9878b7c04218959f9794352eb07dd
Minhee <49539711+minkim7704@users.noreply.github.com>
2023-07-13 18:21:21 +0900

    Update 직사각형 별찍기.py

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.py
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.py
@@ -1,6 +1,8 @@
 a, b = map(int, input().strip().split(' '))
 
-for row in range(b):
-    for col in range(a):
-        print("*", end= "")
-    print()
+for rows in range(b):
+	for star in range(a):
+		print("*", end="")
+        # end="" 는 프린트하고 나면 계속 다음줄로 넘어가서 안넘어가게 하려고
+	print() 
+ # 한줄에 30개 찍으면 다음줄로 넘어가고 다음 줄 별찍으려고
