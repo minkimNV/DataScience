1724fa9d40975ed2a9d6ef4cd9c1a8de5b363d52
Minhee <49539711+minkim7704@users.noreply.github.com>
2023-07-16 18:12:21 +0900

    [level 1] Title: 소수 찾기, Time: 2921.33 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -39,37 +39,79 @@ for i in range(2, n+1):
 담날 푸렀다 하하! 근데 출력 초과가 뜸. 킹받누ㅠ
 '''
 
+# import math
+
+# def solution(n):
+#     answer = []
+#     array = [True for i in range(n+1)]
+#     # print(array)
+#     # n 까지 범위 내의 숫자들을 True로 array 만들어준다.
+
+#     for i in range(2, int(math.sqrt(n))+1):
+#                         # match.sqrt(n)은 n에 루트를 씌워준 값이다.
+#         # 2부터 루트n+1 까지의 정수 중에
+#         if array[i] == True:
+#             # 만약 i번째 원소가 True 라면 (다 true니까 2부터 )
+#             # 0, 1번째 원소는 소수에 해당하지 않으니까 인덱스 2부터 돌린다.
+#             j = 2
+#             # j가 2일 때부터 while - i * j가 보다 n보다 작거나 같으면
+#             while i * j <= n:
+#                 array[i*j] = False
+#                 j+=1
+#                 # i의 배수를 없애는 과정이다.
+#         # 여기 If문을 i = 2 일 때 j = 2 부터 i*j 값이 n보다 작아질 때까지만 돌려
+#                         # 4, 6, 8 10 삭제 됐어
+#         # 다했으면 i = 3 일 때 또 반복
+#                         # 6, 9 삭제 됐어
+#         # if & while 문은 끝났어
+
+#     for i in range(2,n+1):
+#         # 0, 1번째 원소는 소수에 해당하지 않으니까 인덱스 2부터 돌린다.
+#         if array[i]:
+#             # i번째 원소가 트루면
+#             answer.append(i) # 추가하고
+
+#     return len(answer)
+#             # 리스트 원소 갯수 뽑느다
+
+
+
+# 위에 있는 풀이가 맞긴 하지만 그냥 한 번 더.. 이것도 되나..ㅠ
+
 import math
+import sys
 
+def prime(n):
+    if n == 1:
+        return False
+    for i in range(2, int(math.sqrt(n)+1)):
+        if n % i == 0:
+            return False
+    return True
+#print(prime(11))
+#print(prime(24))
+
+'''
+<소수 구하기>
+1. 숫자를 입력 받는다.
+2. 입력 받은 숫자가 1이면 소수가 아니다.
+    - if 절을 이용해 입력받은 숫자가 1이라면 소수가 아니므로 false를 반환한다.
+3. 입력 받은 숫자가 다른 숫자로 나뉘면 소수가 아니다.
+    - 소수의 제곱근은 자기 자신으로만 나뉜다. 따라서 제곱근을 사용해 for문을 돌린다.
+    - 0 과 1은 소수가 될 수 없으므로 2부터 시작한다.
+    - 만약 입력 받은 숫자의 제곱근이 다른 숫자로 나뉜다면 소수가 아니다.
+4. 입력 받은 숫자가 다른 숫자로 나뉘지 않는다면 소수다.
+    - 만약 입력 받은 숫자의 제곱근이 다른 숫자로 나뉘지 않는다면 소수다.
+'''
 def solution(n):
-    answer = []
-    array = [True for i in range(n+1)]
-    # print(array)
-    # n 까지 범위 내의 숫자들을 True로 array 만들어준다.
+    answer = 0
+    for i in range(2, n+1):
+        if prime(i):
+            answer += 1
+    return answer
 
-    for i in range(2, int(math.sqrt(n))+1):
-                        # match.sqrt(n)은 n에 루트를 씌워준 값이다.
-        # 2부터 루트n+1 까지의 정수 중에
-        if array[i] == True:
-            # 만약 i번째 원소가 True 라면 (다 true니까 2부터 )
-            # 0, 1번째 원소는 소수에 해당하지 않으니까 인덱스 2부터 돌린다.
-            j = 2
-            # j가 2일 때부터 while - i * j가 보다 n보다 작거나 같으면
-            while i * j <= n:
-                array[i*j] = False
-                j+=1
-                # i의 배수를 없애는 과정이다.
-        # 여기 If문을 i = 2 일 때 j = 2 부터 i*j 값이 n보다 작아질 때까지만 돌려
-                        # 4, 6, 8 10 삭제 됐어
-        # 다했으면 i = 3 일 때 또 반복
-                        # 6, 9 삭제 됐어
-        # if & while 문은 끝났어
-
-    for i in range(2,n+1):
-        # 0, 1번째 원소는 소수에 해당하지 않으니까 인덱스 2부터 돌린다.
-        if array[i]:
-            # i번째 원소가 트루면
-            answer.append(i) # 추가하고
-
-    return len(answer)
-            # 리스트 원소 갯수 뽑느다
+'''
+m ~ n 사이에 있는 소수를 출력한다.
+1. m, n을 입력받는다.
+2. m부터 n+1까지 범위에 있는 i가 소수라면 i를 출력한다. 
+'''
