c7b8c6de2592f419983cefea70145af8b2979823
Minhee <49539711+minkim7704@users.noreply.github.com>
2023-07-18 17:02:26 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,4 +1,4 @@
-# def solution(s):
+def solution(s):
     # answer = ''
     # s = s.replace(" ", "-")
     # s = ",".join(s).split(",")
@@ -11,29 +11,13 @@
     # answer = answer.replace("-", " ")
     # return answer
 
-#     answer = ''
-#     s = s.split(" ")
-#     for word in s:
-#         for i in range(len(word)):
-#             if i % 2 == 0:
-#                 answer += word[i].upper()
-#             else:
-#                 answer += word[i].lower()
-#         answer += ' '
-#     return answer.strip()
-
-# print(solution("try hello world"))
-
-def solution(s):
     answer = ''
-    new_list = s.split(' ')
-    for i in new_list:
-        for j in range(len(i)):
-            if j % 2 == 0:
-                answer += i[j].upper()
+    s = s.split(" ")
+    for word in s:
+        for i in range(len(word)):
+            if i % 2 == 0:
+                answer += word[i].upper()
             else:
-                answer += i[j].lower()
-        answer+= ' '
+                answer += word[i].lower()
+        answer += ' '
     return answer[0:-1]
-
-print(solution("try hello world"))
