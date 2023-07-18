906d3b00b5a3eef33870d9cfac914fa0b4fe053f
Minhee <49539711+minkim7704@users.noreply.github.com>
2023-07-18 16:04:41 +0900

    Update 3진법 뒤집기.py

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -4,13 +4,13 @@ def solution(n):
     while n > 0:
         n, mod = divmod(n, 3)
         base3 += str(mod)
-    # 여기까지 3진법 변환
+    # 여기까지 3진법 변환. n의 3진법
 
     reverse_base3 = base3[::-1]
+    # 앞뒤반전의 3진법
     base10 = int(base3, 3)
+    # 3진법의 base3을 10진수로 바꾸는 기본 함수
 
     return base10
-    # 역순인 진수를 뒤집어 줘야 원래 변환 하고자하는 base가 출력
+    
 
-
-print(solution(45))
