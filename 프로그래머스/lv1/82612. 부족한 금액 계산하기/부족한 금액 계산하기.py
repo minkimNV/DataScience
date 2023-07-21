6f7117f3c4da902b5968d3b1794ed22a6d0aaa24
Minhee <49539711+minkim7704@users.noreply.github.com>
2023-07-21 21:02:03 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.12 ms, Memory: 9.91 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -1,11 +1,27 @@
+'''
+1. price = 놀이 기구 이용료
+   n * price = 놀이 기구 n 번 째 이용료
+   count = n
+2. 놀이기구를 count 번 타게 됐을 때 가지고 있는 금액에서 얼마가 모자라는지 return
+3. 안모자라면 0 return
+'''
 def solution(price, money, count):
     answer = 0
-    for c in range(1, count + 1):
-        answer = answer + (price * c)
-
-    if money > answer:
-        answer = 0
+    for n in range(1, count+1):
+        answer += n * price
+        # answer 은 놀이기구 탈 만큼 필요한 돈 합이다.
+        
+    if answer > money:
+        answer = answer - money
     else:
-        answer -= money
-
+        answer = 0
+        
     return answer
+
+'''
+- 내 풀이 -
+1. money - (1*price + 2*price + ... + n*price)
+2. 놀이기구 타려면 필요한 돈 = for n in range(1, count+1)
+3. if 전재산이 모자라 -> 얼마나 모자라? return , 안모자라? 0 return
+'''
+
