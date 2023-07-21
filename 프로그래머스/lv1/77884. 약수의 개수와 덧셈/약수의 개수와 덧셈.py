9344ec6ad949a87abd88f2ce9dbe914e1c03b430
Minhee <49539711+minkim7704@users.noreply.github.com>
2023-07-21 21:14:25 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.43 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,20 +1,34 @@
-# 약수를 구하는 방법
-    # 정수 n 을 1 ~ n 에 해당하는 숫자로 나누었을 때 나머지가 0이 된다면 정수 n 의 약수이다.
-    # 예를 들어,  28을 1 부터 28 까지 숫자로 나누었을 때 나머지가 0이면 28의 약수이다.
+'''
+1. 정수: left <= n <= right
+2. left부터 right까지의 모든 수의 약수 중에
+3. 약수의 개수가 짝수인 수는 더하고
+4. 약수의 개수가 홀수인 수는 뺀다.
+'''
 
-def count(n):    # 약수의 합을 구하는 함수 solution이 받는 매개변수는 정수 n 이다.
-    answer = 0      # 약수의 합을 넣을 변수를 정의했다.
-    for i in range(1, n + 1):        # 정수 n을 1부터 n에 해당하는 숫자(range(1, n+1))로 나누는 반복 작업(for)을 통해 약수(i)를 찾을 것이다.
-        if n % i == 0:               # 정수 n을 i로 나누었을 때 나머지가 0 이라면, (i는 n의 약수에 해당하므로)
-            answer += 1     # 약수의 갯수 추가 
-    return answer                    # if 절이 끝나면 for문이 약수의 갯수를 반환
+def divisor(n):
+    divisor_cnt = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            divisor_cnt += 1
+    return divisor_cnt
+'''
+- 내 풀이 1 : 일단 약수의 개수를 구한다 -
+1. n % (1 ~ n+1) == 0 -> 약수다.
+2. i = (1 ~ n+1)
+3. 약수면 divisor_cnt 1 추가
 
+- 풀이 2 : 이제 약수 개수 (짝수 vs. 홀수) 따라서 정수 덧셈 뺄셈 -
+1. left <= 정수 n <= right -> for n in range(left, right+1)
+2. 약수 갯수 짝수면 ( % 2 == 0)
+3. 약수 갯수 홀수는 % 3 == 0 아님. 처음에 홀수를 저렇게 썼다가 5가 짝수 취급당하길래 뭐지? 했다. 걍 너무 바보같은 실수였음 허허
+    짝수가 아니면! 다 홀수지! 마찌!
+'''
 def solution(left, right):
     answer = 0
-    num = [i for i in range(left, right+1)]
-    for i in range(len(num)):
-        if count(num[i]) % 2 == 0:
-            answer += num[i]
+    for n in range(left, right + 1):
+        if divisor(n) % 2 == 0:
+            answer += n
         else:
-            answer -= num[i]
+            answer -= n
+            
     return answer
