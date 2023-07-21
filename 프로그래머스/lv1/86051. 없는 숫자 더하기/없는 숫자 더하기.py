536fccedfd80604b85c6cc13d0216c98f220fcfb
Minhee <49539711+minkim7704@users.noreply.github.com>
2023-07-21 20:50:53 +0900

    Update 없는 숫자 더하기.py

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,6 +1,17 @@
+'''
+1. numbers = [] = 0부터 9까지의 정수 배열
+2. numbers에 없는 숫자를 모두 찾아 더한 수를 return 
+'''
+
 def solution(numbers):
-    # numbers.sort()
     tmp = [i for i in range(10)]
-    if sum(numbers) != sum(tmp):
-        answer = sum(tmp) - sum(numbers)
+    answer = sum(tmp) - sum(numbers)
     return answer
+
+'''
+- 내 풀이 - 
+1. sum(0~9) - sum(numbers) = return
+2. list = [i for i in range(10)]
+
+간단스..
+'''
