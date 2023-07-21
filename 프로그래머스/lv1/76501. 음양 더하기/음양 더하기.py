a8bcbc281aef0ec2738c06dfd71606c553f01913
Minhee <49539711+minkim7704@users.noreply.github.com>
2023-07-21 21:27:34 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -1,3 +1,30 @@
+'''
+1. absolutes = [] = 정수들의 절댓값 배열
+2. signs = [] = 양수냐 음수냐 불리언 배열
+3. 양수면 true, 음수면 false
+4. absolutes 실제 값들의 합 return
+'''
+
+def solution(absolutes, signs):
+    answer = 0
+    for pair in zip(absolutes, signs):
+        if pair[1] == True:
+            answer += pair[0]
+        else:
+            answer -= pair[0]
+    return answer
+
+'''
+- 내 풀이 -
+1. 두 배열 짝지어 준다. zip(absolutes, signs)
+2. 양수냐 음수냐 찾고 바로 더해주기 for 문을 사용
+'''
+
+
+
+'''
+- 처음 시도에 만든 함수 : 이것도 정답이다. -
+
 def solution(absolutes, signs):
     ans = 0
     for i in range(len(signs)):
@@ -6,3 +33,4 @@ def solution(absolutes, signs):
         else:
             ans -= absolutes[i]
     return ans
+'''
