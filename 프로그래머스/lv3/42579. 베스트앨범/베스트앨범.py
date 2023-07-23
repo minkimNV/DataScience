62ce8a501a5d9e49d61a8c585cb5af310dda3242
Minhee <49539711+minkim7704@users.noreply.github.com>
2023-07-23 14:27:36 +0900

    Update 베스트앨범.py

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -6,27 +6,56 @@ def solution(genres, plays):
 
     # 딕셔너리 = {장르: [(재생횟수, 고유번호), (재생횟수, 고유번호), ...]}
     for x in zipping:
-        genre, play_count, unique_id = x[0], x[1], x[2]
-        hashbrown[genre].append((play_count, unique_id))
-    # print(hashbrown)
+        genre, play, id = x[0], x[1], x[2]
+        hashbrown[genre].append((play, id))
+
     # 총 재생 횟수 기준 장르 정렬
     sorted_genres = sorted(hashbrown.keys(),
-                           key=lambda genre: sum(play_count
-                                                 for play_count, _ in hashbrown[genre]),
-                           reverse=True)
-    # print(sorted_genres)
+                           key=lambda genre: sum(play for play, _ in hashbrown[genre]), reverse=True)
     
     # 장르 내에서 상위 2곡의 인덱스 추출
     answer = []
     for genre in sorted_genres:
-        # print(genre)
-        top_2_songs = [unique_id for _, unique_id
-                       in sorted(hashbrown[genre],
-                                 key=lambda x: (-x[0], x[1]))[:2]]
-        # print(top_2_songs)
-        answer.extend(top_2_songs)
+        top2 = [id for _, id in sorted(hashbrown[genre], key=lambda x: (-x[0], x[1]))[:2]]
+        answer.extend(top2)
 
     return answer
 
 # 예시 테스트
 # print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]))
+
+
+
+def solution(genres, plays):
+
+    songs = [[genre, (play, i)] for genre, (play, i) in zip(genres, enumerate(plays))]
+    songs = sorted(songs, key = lambda x: (x[0], -x[1][1], x[1][0]))
+
+
+    # 재생횟수 많은 장르 찾기
+    gorder = {}
+    for i in range(len(songs)):
+        if songs[i][0] not in gorder:
+            gorder[songs[i][0]] = songs[i][1][1]
+        else:
+            gorder[songs[i][0]] += songs[i][1][1]
+
+    sortedgorder = sorted(gorder.items(), key=lambda x: -x[1])
+
+    # 상위 2 곡 인덱스
+    answer = []
+    cnt = 0
+    for genre in sortedgorder:
+        cnt = 0
+        for song in songs:
+            if genre[0] == song[0]:
+                cnt += 1
+                if cnt > 2 :
+                    break
+                else:
+                    answer.append(song[1][0])
+
+    return answer
+
+print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]))
+
