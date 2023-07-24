8e4ee70c58d113fa55dce609303d8b51fb185a34
Minhee <49539711+minkim7704@users.noreply.github.com>
2023-07-24 21:38:16 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,27 +1,20 @@
-# 홀수 짝수
-    # def solution(num):
-    #     if num % 2 == 0:
-    #         return "Even"
-    #     else:
-    #         return "Odd"
-
-
 def solution(n):
+    if n == 1 :
+        return 0
+
     cnt = 0
-    while n != 1:
+    while n != 1 and cnt != 500:
         cnt += 1
         if n % 2 == 0:
-            n = n / 2
-            print(n)
+            n = n/2
         else:
-            n = n * 3 + 1
-            print(n)
-            continue
+            n = (n * 3) + 1
 
-        if n == 1:
-            break
-
-        if cnt >= 500 and n != 1:
-            cnt = -1
-            break
+        if cnt == 500:
+            return -1
     return cnt
+
+
+# print(solution(6))
+# print(solution(16))
+# print(solution(626331))
