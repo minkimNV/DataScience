307a53a04f091caa68ec441f471ea3b3399c2098
Minhee <49539711+minkim7704@users.noreply.github.com>
2023-08-29 19:21:48 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -5,8 +5,7 @@
     - 1번 3번이 잃어버리고 2번 4번이 여벌이 있다
     - n-1에게 우선적으로 빌려준다
     - 3번 5번이 잃어버리고 2번 4번이 여벌이 있다
-    - 2번은 우선적으로 n-1번한테 빌려주려고 한다. n-1이 없어서 n+1한테 빌려준다. 4번도 우선적으로 n-1번한테 빌려주려고 한다. ... 
-    -> 한번 빌려준 애는 여벌 배열에서 빼고 빌린 애도 도난 배열에서 빼야 한다.
+    - 2번은 우선적으로 n-1번한테 빌려주려고 한다. n-1이 없어서 n+1한테 빌려준다. 4번도 우선적으로 n-1번한테 빌려주려고 한다. ... -> 한번 빌려준 애는 여벌 배열에서 빼고 빌린 애도 도난 배열에서 빼야 한다.
 2. 여벌 학생이 도난 당했을 수 있다 -> 도난 배열, 여벌 배열에서 여벌 학생 삭제
 n = 학생 수
 lost = [도난 학생 배열]
@@ -18,22 +17,20 @@ def solution(n, lost, reserve):
     lost.sort()
     reserve.sort()
     # 1. 여벌 학생이 도난 당했을 수 있으므로 여벌 학생을 제거한 새로운 도난 배열 생성 및 추가
-    new_lost = []
-    for student in lost:
-        if student not in reserve:
-            new_lost.append(student)
+    # new_lost = []
+    # for student in lost:
+    #     if student not in reserve:
+    #         new_lost.append(student)
 
-    # 더 간단히
-    # new_lost = [student for student in lost if student not in reserve]
+    new_lost = [student for student in lost if student not in reserve]
 
     # 2. 여벌 학생이 도난 당했을 수 있으므로 여벌 학생을 제거한 새로운 여벌 배열 생성 및 추가
-    new_reserve = []
-    for student in reserve:
-        if student not in lost:
-            new_reserve.append(student)
+    # new_reserve = []
+    # for student in reserve:
+    #     if student not in lost:
+    #         new_reserve.append(student)
 
-    # 더 간단히
-    # new_reserve = [student for student in reserve if student not in lost]
+    new_reserve = [student for student in reserve if student not in lost]
 
     # 3. 빌려주기 시작
     for i in new_reserve:
@@ -48,18 +45,3 @@ def solution(n, lost, reserve):
 # print(solution(5, [2,4], [1,3,5]))
 # print(solution(5, [3, 5], [2, 4]))
 # print(solution(5, [2, 3, 5], [2, 3, 4]))
-
-
-
-
-'''
-신기한 거 (?) 발견
-사실 인덱스로 뭔가를 하는 게 아니라 sort 가 필요 없는 것 같아서 안했는데
-테스트 한두개?에서 실패가 떴다.
-반례를 찾아서 해봤는데도 답이 잘 나오는데?? 뭘까
-설마 sort때문인가? 했는데 sort때문..;
-그래서
-lost.sort()
-reserve.sort()
-해주고 시작했당. 쩝.
-'''
